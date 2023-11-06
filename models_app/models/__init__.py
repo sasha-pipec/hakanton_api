ec33ec1891c7f7dad1cc6f135315ec3d10f9d367
sasha-pipec <razarenov.aleksandar@yandex.ru>
2023-11-06 11:53:09 +0300

    added import in init models_app< added auth_user_model in djnago settings

--- models_app/models/__init__.py
+++ models_app/models/__init__.py
@@ -3,3 +3,6 @@ from models_app.models.answer.models import Answer
 from models_app.models.event.models import Event
 from models_app.models.message.models import Message
 from models_app.models.user_card.models import UserCard
+from models_app.models.user.models import User
+from models_app.models.card.models import Card
+from models_app.models.room.models import Room
