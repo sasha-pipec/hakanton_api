e3bac62f75c723280bba7847c4b62f20041c85c5
Aplles <karaichevmaksim@gmail.com>
2023-11-07 12:06:20 +0300

    add re

---
+++ api/services/user/step.py
@@ -0,0 +1,23 @@
+from django import forms
+from service_objects.fields import ModelField
+from service_objects.services import ServiceWithResult
+
+from models_app.models import User
+
+
+class UserStepService(ServiceWithResult):
+    position = forms.IntegerField()
+    user = ModelField(User)
+
+    def process(self):
+        self._position()
+        self.result = self._user
+        return self
+
+    def _position(self):
+        self._user.position = self.cleaned_data['position']
+        self._user.save()
+
+    @property
+    def _user(self):
+        return self.cleaned_data['user']
