7033775228c1e3d480118f6bad6e979a9a263671
sasha-pipec <razarenov.aleksandar@yandex.ru>
2023-11-07 13:20:58 +0300

    added new field to response join user into room

--- api/serializers/room/serializers.py
+++ api/serializers/room/serializers.py
@@ -6,12 +6,18 @@ from models_app.models import Room
 class RoomSerializer(serializers.ModelSerializer):
     count_players_now = serializers.SerializerMethodField()
     in_room = serializers.SerializerMethodField()
+    users = serializers.SerializerMethodField()
 
     def get_count_players_now(self, obj):
         return obj.users.all().count()
 
     def get_in_room(self, obj):
         return obj.users.filter(id=self.context['user'].id).exists()
+
+    def get_users(self, obj):
+        if obj.count_players == obj.users.all().count():
+            return [user.id for user in obj.users.all()]
+        return None
 
     class Meta:
         model = Room
@@ -21,4 +27,5 @@ class RoomSerializer(serializers.ModelSerializer):
             'count_players',
             'count_players_now',
             'in_room',
+            'users'
         )
